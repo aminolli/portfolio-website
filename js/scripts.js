e43dbe7fc16b8dcb090c9dc3852ccb6e5bbca599
aminolli <82242965+aminolli@users.noreply.github.com>
2021-06-11 19:47:03 -0700

    Menu styling and js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi There!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
